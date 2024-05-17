7ebfdddef41d4ad4eb47628acebe2d2e908e63d6
Roman Choporov <40148652+Delrik@users.noreply.github.com>
2024-05-17 02:38:06 +0200

    Play sounds on break depending on break time (#11)

--- lua/Break.lua
+++ lua/Break.lua
@@ -10,10 +10,18 @@ local plugin = SJ.Break
 local callbacks = SJ.CallbackRegister
 local sounds = SJ.SoundAssets:init(SJ.SoundAssets.BreakSounds)
 
-function plugin.callback(_, _, arg)
+function plugin.callback(_, _, arg, seconds)
     if arg == "Break time!" and not plugin.announced then
         plugin.announced = true
-        PlaySoundFile(sounds:choose(), "Master")
+        local sound
+        if seconds == 60 then
+            sound = sounds:choose(1)
+        elseif seconds == 180 then
+            sound = sounds:choose(2)
+        else
+            sound = sounds:choose()
+        end
+        PlaySoundFile(sound, "Master")
         -- Handle sound duplication due to 2 callbacks being fired from DBM one right after the other
         C_Timer.After(5, function()
             plugin.announced = false

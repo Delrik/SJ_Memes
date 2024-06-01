5ca733c9542fcd76f1dd30df6e2a53d2c935f48f
Roman Choporov <ChoporovRoman@gmail.com>
2024-06-01 15:56:44 -0700

    Add return after scheduling

--- lua/ExecuteAlert.lua
+++ lua/ExecuteAlert.lua
@@ -23,6 +23,7 @@ function plugin:repeater()
     if not hp then
         -- Reschedule. This is a workaround for cases when hp is not available for a moment but the combat is still ongoing
         C_Timer.After(0.1, plugin.repeater)
+        return
     end
     if hp > plugin.threshold then
         plugin.notified = false

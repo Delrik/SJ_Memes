17280195f37ea58df0d89dbb62537c45bbbfb6e2
Roman Choporov <40148652+Delrik@users.noreply.github.com>
2024-07-09 03:19:50 +0200

    Implement options system (#15)

--- lua/SJ_Core.lua
+++ lua/SJ_Core.lua
@@ -1,9 +1,2 @@
 SJ = SJ or {}
 SJ.addon_name = "SJ_Memes"
-
-SJ.OnWipe:enable(true)
-SJ.OnExecuteStage:enable(true)
-SJ.OnKill:enable(true)
-SJ.OnNinjaPull:enable(true)
-SJ.OnResurrect:enable(true)
-SJ.OnBreak:enable(true)
--- lua/SavedVariables.lua
+++ lua/SavedVariables.lua
@@ -6,15 +6,19 @@ local events = SJ.EventRegister
 
 function plugin.OnLoadEvent(arg1)
     if arg1 == SJ.addon_name then
-        -- OnExecuteStage
-        if not ExecuteThreshold then ExecuteThreshold = 35 end
-        SJ.OnExecuteStage:set_threshold(ExecuteThreshold)
+        SJ.Options:set_options(SJ_Options)
+
+        -- TODO: Remove this in the future
+        -- Intented for backward compatibility
+        if ExecuteThreshold then
+            SJ.OnExecuteStage:set_threshold(ExecuteThreshold)
+        end
     end
 end
 
 function plugin.OnLogoutEvent()
     -- OnExecuteStage
-    ExecuteThreshold = SJ.OnExecuteStage:get_threshold()
+    SJ_Options = SJ.Options:get_options()
 end
 
 events:register("ADDON_LOADED", plugin.OnLoadEvent)
